600929ce6267b982a66fea26a78dac3392c14995
Antonimason <elchuli3000@gmail.com>
2023-09-04 20:25:39 +0100

    3rd image added

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,17 +1,18 @@
 import "./About.css";
 import Luxury from "../../Assets/luxury.jpg";
 import Athenia from "../../Assets/athenia.jpg";
+import Sonrie from "../../Assets/sonrie.jpg";
 
 function About () {
     return (
         <div className="about">
-            <h2 className="about__title">Acerca de mi</h2>
             <div className="about__pictures">
                 <img className="about__img"src={Luxury} alt="Luxury" height="100px" width="400px" />
                 <img className="about__img" src={Athenia} alt="Athenia" height="100px" width="400px" />
+                <img className="about__img" src={Sonrie} alt="Sonrie" height="100px" width="400px" />
             </div>
             <div className="about__text">
-                <h3 className="about__subtitle">Brigadeiro</h3>
+                <h2 className="about__title">Acerca de mi</h2>
                 <p className="about__description">ipsum uashfiusha saufhasiu  ijhnfqiujas iuuwqfdu jsnfjsan jqwnf jqhwisoia jsajifnsaiuj nsi ujnfasis ijnsa ijsfsjaifijsafhjsaoi isajfnas ijasnfsa  ijasnfsaifsanfn isjfnsafhjn hjfnsa hjfnsnf hssh h shs hsshwyu jsajfsafhb jsfnaskfjyuw</p>
                 <h4 className="about__slogan">acompa;ame a ver mis servicios</h4>
             </div>
