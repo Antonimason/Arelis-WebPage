3d0a13b013de2e9674453e1feb9184e7ce40db90
Antonimason <elchuli3000@gmail.com>
2023-09-12 20:12:24 +0100

    hover added

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -8,7 +8,7 @@ function Home() {
             <div className="home__text">
                 <h1 className="home__title">Arelis Guevara</h1>
                 <p className="home__subtitle">Asesoría de imagen personal y coach</p>
-                <div className="home__imgtext"><img src={Acce} alt="modelo 2" className="home__picturetext"/></div>
+                <div className="home__imgText"><img src={Acce} alt="modelo 2" className="home__picturetext"/></div>
             </div>
             <div className="home__img"><img src={imagenHome} alt="modelo 1" className="home__picture"/></div>
         </div>
